e569244d3bdd8ba722d3805e69a0f431822a064b
louislowjk <36294889+louislowjk@users.noreply.github.com>
2021-08-14 00:42:22 +0800

    Add files via upload

---
+++ SoftwareEngineerAssessment/src/main/java/com/ryde/SoftwareEngineerAssessment/SoftwareEngineerAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.ryde.SoftwareEngineerAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEngineerAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareEngineerAssessmentApplication.class, args);
+	}
+
+}
