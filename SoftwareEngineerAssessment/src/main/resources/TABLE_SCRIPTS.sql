99dd3035e0fc984d4b18502123c7641eb75a7ebf
Louis <louislowjk@hotmail.com>
2021-08-17 13:05:41 +0800

    Added some default data in TABLE_SCRIPTS.sql

--- SoftwareEngineerAssessment/src/main/resources/TABLE_SCRIPTS.sql
+++ SoftwareEngineerAssessment/src/main/resources/TABLE_SCRIPTS.sql
@@ -13,4 +13,10 @@ CREATE TABLE USER(
     Description VARCHAR(200),
     CreatedAt DATE
     
-)
+);
+
+insert into User values(1,"Jack", "1994-03-29","Bukit Timah Road","This is a description" ,"2021-07-01");
+insert into User values(2,"John", "1999-12-30","Seah Im Street","This is a description" ,"2021-07-11");
+insert into User values(3,"Mary", "1989-04-09","50 Bugis road ","This is a description" ,"2021-07-19");
+insert into User values(4,"Kalebh", "1993-09-05","Cross street","This is a description" ,"2021-07-11");
+insert into User values(5,"Joshua", "1984-04-11","Chinatown","This is a description" ,"2021-07-11");
